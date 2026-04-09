4bb15df9a8c61f34c7e749dd80de97254265cf2e
Mher <mhergrigur@gmail.com>
2026-04-09 13:35:59 +0400

    add commented wine pkgs

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,6 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.steam = {
     enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
+
+  # -------------- windows support
+  # winetricks
+  # wineWow64Packages.stable
+  # wineWow64Packages.waylandFull
+  # bottles
+  # ----------------------------------
+
+  # https://github.com/ElyPrismLauncher/ElyPrismLauncher
+
 }
